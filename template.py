980135ce999c8aa18d2543e69c3bfb70cfe93c9d
Aakash Yadav <Aakashsyadav1999@gmail.com>
2025-01-25 11:42:20 +0530

    Initial Commit

---
+++ template.py
@@ -0,0 +1,51 @@
+import os
+
+list_of_files = [
+    
+    'setup.py',
+    'requirements.txt',
+    'README.md',
+    'LICENSE',
+    'MANIFEST.in',
+    'Makefile',
+    'Dockerfile',
+    '.dockerignore',
+    '.gitignore',
+    '.gitattributes',
+    '.editorconfig',
+    '.bumpversion.cfg',
+    'research/research.ipynb',
+    'docker-compose.yml',
+    'app.py',
+    'src/__init__.py',
+    'src/constant/__init__.py',
+    'src/utils.py/__init__.py',
+    'src/utils.py/common.py',
+    'src/models.py',
+    'src/prompts/__init__.py',
+    'src/prompts/prompts.py',
+    'models/config.yml',
+    'database/config.yml',
+    'src/exception/__init__.py',
+    'src/logger/__init__.py',
+]
+
+
+def create_files():
+    for file in list_of_files:
+        directory = os.path.dirname(file)
+        if directory and not os.path.exists(directory):
+            os.makedirs(directory)
+        if not os.path.exists(file):
+            with open(file, 'w') as f:
+                f.write('')
+            print(f'Created {file}')
+        else:
+            print(f'{file} already exists')
+            
+
+def main():
+    create_files()
+
+if __name__ == '__main__':
+    main()
