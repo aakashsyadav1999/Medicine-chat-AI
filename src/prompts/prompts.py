bd4f3164a1e0d98928957d6b914b73aa95f6bdb6
Aakash Yadav <Aakashsyadav1999@gmail.com>
2025-01-30 00:09:33 +0530

    Changes in initial prompt

--- src/prompts/prompts.py
+++ src/prompts/prompts.py
@@ -5,7 +5,7 @@ class Prompt:
 
     def prompt_main(self):
 
-        prompt_text = """Please analyze the attached photo of the medicine. Provide the advantages and disadvantages of this medicine, and explain why a doctor
+        prompt_text = """Please analyze the attached photo of the medicine. Extract the proper name of the medicine Provide the advantages and disadvantages of this medicine, and explain why a doctor
                         might prescribe this specific medicine. Ensure that your response is strictly limited to medical terms and does not include any other information."""
 
         return prompt_text
